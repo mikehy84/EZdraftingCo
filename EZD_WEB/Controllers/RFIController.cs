a833e37de2e8f36b97c8bfa5eb02de1f2e9a78b0
Mike <mike.hy84@gmail.com>
2025-04-10 23:06:01 -0700

    started working on UserPanel

---
+++ EZD_WEB/Controllers/RFIController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EZD_WEB.Controllers
+{
+    public class RFIController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
