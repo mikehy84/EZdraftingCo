8c8993cff82ad6d183c98e30899c5b0ea7107f23
Mike <mike.hy84@gmail.com>
2025-04-21 09:31:10 -0700

    spinner added for loading

---
+++ EZD_WEB/wwwroot/js/spinner.js
@@ -0,0 +1,21 @@
+﻿$(document).ready(function () {
+    // For form submission
+    $("form").on("submit", function () {
+        $("#loader").show();
+    });
+
+    // Optional: For link clicks
+    $("a").on("click", function (e) {
+        const href = $(this).attr("href");
+        if (href && !href.startsWith("#") && !href.startsWith("javascript:")) {
+            $("#loader").show();
+        }
+    });
+
+    // Optional: For AJAX
+    $(document).ajaxStart(function () {
+        $("#loader").show();
+    }).ajaxStop(function () {
+        $("#loader").hide();
+    });
+});
